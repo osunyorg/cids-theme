bde18cc9c668c69a79ef450c604e1b6c26695250
alexisben <alex@noesya.coop>
2024-10-30 16:16:54 +0100

    js & theme

---
+++ assets/js/toc.js
@@ -0,0 +1,23 @@
+window.cids = window.cids || {};
+
+window.cids.Toc = function () {
+  this.summary = document.querySelector('.extended-summary');
+  this.floatingToc = document.querySelector('.document-content > .toc-cta');
+  window.addEventListener('scroll', this.onScroll.bind(this))
+};
+
+window.cids.Toc.prototype = {
+  onScroll: function () {
+    var rect = this.summary.getBoundingClientRect();
+
+    if (rect.y < -rect.height) {
+      this.floatingToc.classList.add('is-visible');
+    } else {
+      this.floatingToc.classList.remove('is-visible');
+    }
+  }
+};
+
+window.addEventListener('load', function () {
+  var cidsToc = new window.cids.Toc();
+});
