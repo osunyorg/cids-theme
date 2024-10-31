f42f4ae7a65d44501306397b0293d86eae28cc03
Olivia206 <olivia.simonet206@gmail.com>
2024-10-31 01:15:14 +0100

    fixed js jump

--- assets/js/home-parallax.js
+++ assets/js/home-parallax.js
@@ -1,42 +1,45 @@
 window.cids = window.cids || {};
 
 window.cids.HomeParallax = function () {
-    this.hero = document.querySelector('.page__home .hero');
+    this.hero = document.querySelector('.page__home .hero--with-image');
     this.imageContainer = this.hero.querySelector('figure');
     this.titleContainer = this.hero.querySelector('.hero-text');
     this.title = this.hero.querySelector('h1');
-
-    if (!this.imageContainer) {
-        return;
-    }
 
     this.listen();
 };
 
 window.cids.HomeParallax.prototype = {
     listen: function() {
+        this.checkScrollPosition();
         window.addEventListener('scroll', this.onScroll.bind(this));
     },
     onScroll: function () {
-        var scrollLimit = this.imageContainer.offsetHeight;
         this.scrollY = window.scrollY;
 
-        if (this.scrollY <= scrollLimit) {
-            this.translateImage();
-            this.hideTitle();
-        }
+        this.translateImage();
+        this.hideTitle();
     },
     translateImage: function() {
         var translateTop = -this.scrollY * 0.3;
         this.imageContainer.style.marginTop = `${translateTop}px`;
     },
     hideTitle: function() {
+        this.title.style.setProperty('position', 'fixed')
         var transitionTrigger = this.title.offsetHeight / 2;
         var opacity = 1 - (this.scrollY - transitionTrigger) / transitionTrigger;
 
         opacity = Math.max(0, Math.min(opacity, 1));
         this.title.style.opacity = opacity;
-    }
+    },
+    checkScrollPosition: function() {
+        this.scrollY = window.scrollY;
+        var scrollLimit = this.hero.offsetHeight;
+        console.log(this.scrollY, scrollLimit)
+        if (this.scrollY > scrollLimit) {
+            this.title.style.setProperty('opacity', '0');
+        }
+    },
 };
 
 window.addEventListener('load', function () {
