aaafe5e1168e46fc91eb90cc4f5bf34d11fb755c
Olivia206 <olivia.simonet206@gmail.com>
2024-11-08 17:14:54 +0100

    added js on scroll

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -14,8 +14,10 @@ window.cids.Testimonial.prototype = {
     listen: function() {
         for (var i = 0; i < this.testimonials.length; i++) {
             this.setClonedPattern(this.testimonials[i]);
-            window.addEventListener('scroll', this.onScroll(this.testimonials[i]).bind(this));
-          }
+            window.addEventListener('scroll', ((element) => {
+                return this.onScroll(element);
+            }).bind(this, this.testimonials[i]));
+        }
     },
     setClonedPattern: function(element) {
         // Get pattern from hook and put it on tesimonials block
@@ -31,8 +33,17 @@ window.cids.Testimonial.prototype = {
         this.patternPath.style.strokeDasharray = this.patternLength;
     },
     onScroll: function (element) {
-        var containerTop = element.getBoundingClientRect().top;
-        
+        var containerTop = element.getBoundingClientRect().top,
+            distance = element.offsetHeight,
+            windowHeight = window.innerHeight;
+
+        if (containerTop < windowHeight && containerTop > -distance) {
+            var scroll = Math.max(0, Math.min(1, (windowHeight - containerTop) / (windowHeight + distance))),
+                offset = this.patternLength * (1 - (scroll * 1.5)); // not sure about that : added speed to display entire pattern before ending animation
+                
+            this.patternPath.style.strokeDashoffset = offset;
+            this.patternPath.style.strokeDasharray = this.patternLength;
+        }
     }
 };
 
