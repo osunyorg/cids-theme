bbd111ef8e01d0ba4907903a2ec52c583c1c750a
Olivia206 <olivia.simonet206@gmail.com>
2026-06-19 11:35:35 +0200

    fixed meta width in persons

--- assets/js/persons-contact.js
+++ assets/js/persons-contact.js
@@ -3,42 +3,56 @@ window.cids = window.cids || {};
 window.cids.ContactDetails = function () {
   this.pageContent = document.querySelector('.document-content');
   this.sidebar = document.querySelector('.section-sidebar');
-  this.personImage = document.querySelector('.hero .content > figure')
+  this.personImage = document.querySelector('.hero .content > figure');
 
-  if (window.innerWidth <= 768) {
+  if (window.innerWidth <= 768 || !this.personImage) {
     return;
   }
-  
-  this.updatePosition();
-  this.updatePosition = this.updatePosition.bind(this);  
-  this.updateContentHeight();
+
+  this.updatePosition = this.updatePosition.bind(this);
+  this.updateContentHeight = this.updateContentHeight.bind(this);
+
+  this.init();
   this.addEventListeners();
 };
 
 window.cids.ContactDetails.prototype = {
+  init: function () {
+    var self = this;
+
+    this.updatePosition();
+    this.updateContentHeight();
+
+    if (window.ResizeObserver) {
+      this.resizeObserver = new ResizeObserver(function () {
+        self.updatePosition();
+        self.updateContentHeight();
+      });
+      this.resizeObserver.observe(this.personImage);
+    }
+  },
+
   updatePosition: function () {
     if (this.sidebar) {
       this.sidebar.style.top = this.personImage.offsetHeight + 'px';
     }
   },
 
-  updateContentHeight: function() {
-    var self = this;
-    var sidebarHeidht = this.sidebar ? this.sidebar.offsetHeight : 0,
-        sidebarHeight = sidebarHeidht + this.personImage.offsetHeight;
-    
-    this.pageContent.style.minHeight = `calc(${sidebarHeight}px)`;
+  updateContentHeight: function () {
+    var sidebarHeight = this.sidebar ? this.sidebar.offsetHeight : 0;
+    sidebarHeight += this.personImage.offsetHeight;
+    this.pageContent.style.minHeight = sidebarHeight + 'px';
   },
-  
+
   addEventListeners: function () {
     var self = this;
-    
-    window.addEventListener('resize', function() {
+
+    window.addEventListener('resize', function () {
       self.updatePosition();
       self.updateContentHeight();
     });
-    
-    window.addEventListener('load', function() {
+
+    window.addEventListener('load', function () {
       self.updatePosition();
       self.updateContentHeight();
     });
